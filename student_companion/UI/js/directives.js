e55c874fb7243f5ca2758d9951006bc7b0744657
sakharamg <213050027@iitb.ac.in>
2021-10-27 15:00:25 +0530

    Documented CSS

--- student_companion/UI/js/directives.js
+++ student_companion/UI/js/directives.js
@@ -1,16 +1,15 @@
 var studentCompanionApp = angular.module('studentCompanionApp');
 
-console.log('from directive')
+
 studentCompanionApp.directive('profile', function() {
     return {
-      templateUrl: 'views/profile.html'
+        templateUrl: 'views/profile.html'
     };
-  });
+});
 
 
-  studentCompanionApp.directive('login', function() {
+studentCompanionApp.directive('login', function() {
     return {
-      templateUrl: 'views/login/index.html'
+        templateUrl: 'views/login/index.html'
     };
-  });
-  
+});
